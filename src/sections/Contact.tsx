bcd66e689e59c9968fd1b6698951ec47ba71e68d
Kasper Luna <dev@kasperluna.com>
2023-03-10 19:24:40 +0800

    fixed form submissions

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,7 +1,91 @@
 import Link from "next/link";
+import { useState, type FormEvent } from "react";
+import { Spinner } from "~/assets/Spinner";
 import Footer from "./Footer";
 
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
 export default function Contact() {
+  const [isSent, setIsSent] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isFailed, setIsFailed] = useState(false);
+
+  const resetError = () => setIsError(false);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    setIsSubmitting(true);
+    e.preventDefault();
+    const form = e.currentTarget as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const data: FormData = {
+      name: formData.get("name") as string,
+      email: formData.get("email") as string,
+      message: formData.get("message") as string,
+    };
+
+    if (!data.name || !data.email || !data.message) {
+      setIsError(true);
+      setIsSubmitting(false);
+      return;
+    }
+    const Body = JSON.stringify({
+      access_key: process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY,
+      ...data,
+    });
+    void fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: Body,
+    })
+      .then((response) => {
+        if (Boolean(response.ok)) {
+          setIsSent(true);
+        } else {
+          setIsFailed(false);
+        }
+      })
+      .catch(() => {
+        setIsFailed(true);
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  };
+
+  const getClassName = () => {
+    if (isSent) {
+      return "mt-5 w-full rounded-md bg-green-300 py-2 px-3 text-center font-semibold text-green-900";
+    } else if (isError || isFailed) {
+      return "mt-5 w-full rounded-md bg-red-300 p-2 text-center font-semibold text-red-900";
+    } else {
+      return "hidden";
+    }
+  };
+
+  const getMessage = () => {
+    if (isSent) {
+      return "Message sent! I'll try my best to get back to you as soon as possible :) Have a Great Day!";
+    } else if (isError) {
+      return "Please make sure everything is filled properly and try again!";
+    } else if (isFailed) {
+      return "An unknown error occurred. Sorry about that! Please contact me via email if this does not resolve on its own.";
+    } else {
+      return "";
+    }
+  };
+
+  const className = getClassName();
+  const message = getMessage();
+
   return (
     <div
       id={"Contact"}
@@ -61,7 +145,7 @@ export default function Contact() {
       <div className="z-20 py-16">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight  text-slate-600 dark:text-slate-200 sm:text-4xl">
-            Let&apos;s Talk!
+            Let&apos;s Talk!{" "}
           </h2>
           <p className="mt-2 text-lg leading-8  text-gray-600 dark:text-gray-400">
             You can always reach me at{" "}
@@ -72,9 +156,8 @@ export default function Contact() {
           </p>
         </div>
         <form
-          action="#"
-          method="POST"
           className="mx-auto mt-12 max-w-xl sm:mt-10"
+          onSubmit={handleSubmit}
         >
           <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
             <div className="sm:col-span-2">
@@ -91,6 +174,8 @@ export default function Contact() {
                   id="email"
                   autoComplete="email"
                   placeholder="johndoe@gmail.com"
+                  onChange={resetError}
+                  disabled={isSent}
                   className="focus:ring-insetdark:bg-slate-800 block w-full rounded-md border-0 py-2 px-3.5 text-gray-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 dark:bg-slate-800 dark:text-gray-400 dark:placeholder:text-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -109,6 +194,8 @@ export default function Contact() {
                   id="name"
                   autoComplete="given-name"
                   placeholder="John Doe"
+                  onChange={resetError}
+                  disabled={isSent}
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-slate-800 dark:text-gray-400 dark:placeholder:text-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -125,21 +212,32 @@ export default function Contact() {
                 <textarea
                   name="message"
                   id="message"
-                  rows={4}
+                  rows={3}
+                  onChange={resetError}
+                  disabled={isSent}
                   placeholder="Hi Kasper! I want to discuss a project with you."
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-slate-800 dark:text-gray-400 dark:placeholder:text-gray-600 sm:text-sm sm:leading-6"
-                  defaultValue={""}
                 />
               </div>
             </div>
           </div>
           <div className="mt-10">
-            <button
-              type="submit"
-              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Send!
-            </button>
+            {!isSent && (
+              <button
+                disabled={isSubmitting}
+                type="submit"
+                className="flex w-full flex-row justify-between rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                <div></div>
+                <p className="place-self-center">Send!</p>
+                <div>
+                  {isSubmitting && (
+                    <Spinner className="w-4 animate-spin fill-black dark:fill-white" />
+                  )}
+                </div>
+              </button>
+            )}
+            <div className={className}>{message}</div>
           </div>
         </form>
       </div>
