11b133e75bbdd5deb4e51d938226682fcf665ecc
Kasper Luna <dev@kasperluna.com>
2023-03-10 18:17:39 +0800

    added footer section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,36 +1,15 @@
 import Link from "next/link";
+import Footer from "./Footer";
 
 export default function Contact() {
   return (
-    <div className="relative isolate gap-4 overflow-hidden bg-white py-24 px-6 dark:bg-slate-900 sm:py-32 lg:px-8">
-      <div className="pointer-events-none absolute inset-x-0 -z-10 transform-gpu overflow-hidden pt-[10rem] blur-[70px] sm:top-[-20rem]">
+    <div
+      id={"Contact"}
+      className="relative isolate flex min-h-[90vh] flex-col justify-between gap-4 overflow-hidden bg-white px-6 dark:bg-slate-900"
+    >
+      <div className="pointer-events-none absolute inset-x-0 top-[-5rem] -z-10 transform-gpu overflow-hidden pl-[40rem] pt-[15vw] blur-[45px] sm:blur-[70px]">
         <svg
-          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-          viewBox="0 0 1155 678"
-        >
-          <path
-            className="fill-sky-800"
-            fillOpacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-          <defs>
-            <linearGradient
-              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-              x1="1155.49"
-              x2="-78.208"
-              y1=".177"
-              y2="474.645"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
-      <div className="pointer-events-none absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden pt-[40rem] blur-[70px] sm:top-[-20rem] ">
-        <svg
-          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2"
           viewBox="0 0 1155 678"
         >
           <path
@@ -53,7 +32,33 @@ export default function Contact() {
           </defs>
         </svg>
       </div>
-      <div className="-z-20">
+      <div className="pointer-events-none absolute inset-x-0 -z-10 transform-gpu overflow-hidden pt-[10rem] blur-[55px] sm:blur-[70px]">
+        <svg
+          className="relative  left-[calc(50%-30rem)] -z-10 h-[42.375rem] max-w-none -translate-x-1/2 rotate-[30deg]"
+          viewBox="0 0 1155 678"
+        >
+          <path
+            className="fill-sky-800"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC" />
+              <stop offset={1} stopColor="#FF80B5" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+
+      <div className="z-20 py-16">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight  text-slate-600 dark:text-slate-200 sm:text-4xl">
             Let&apos;s Talk!
@@ -69,7 +74,7 @@ export default function Contact() {
         <form
           action="#"
           method="POST"
-          className="mx-auto mt-16 max-w-xl sm:mt-20"
+          className="mx-auto mt-12 max-w-xl sm:mt-10"
         >
           <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
             <div className="sm:col-span-2">
@@ -85,7 +90,8 @@ export default function Contact() {
                   name="email"
                   id="email"
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:text-gray-400 sm:text-sm sm:leading-6"
+                  placeholder="johndoe@gmail.com"
+                  className="focus:ring-insetdark:bg-slate-800 block w-full rounded-md border-0 py-2 px-3.5 text-gray-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 dark:bg-slate-800 dark:text-gray-400 dark:placeholder:text-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -102,7 +108,8 @@ export default function Contact() {
                   name="name"
                   id="name"
                   autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:text-gray-400 sm:text-sm sm:leading-6"
+                  placeholder="John Doe"
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-slate-800 dark:text-gray-400 dark:placeholder:text-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -119,7 +126,8 @@ export default function Contact() {
                   name="message"
                   id="message"
                   rows={4}
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Hi Kasper! I want to discuss a project with you."
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-slate-800 dark:text-gray-400 dark:placeholder:text-gray-600 sm:text-sm sm:leading-6"
                   defaultValue={""}
                 />
               </div>
@@ -135,6 +143,7 @@ export default function Contact() {
           </div>
         </form>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/sections/Footer.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { ButtonLink } from "~/components/ButtonLink";
+import { Tooltip } from "~/components/Tooltip";
+import { GithubIcon } from "~/assets/GithubIcon";
+import { LinkedInIcon } from "~/assets/LinkedInIcon";
+import { KasperBoxLogo } from "~/assets/KasperBoxLogo";
+
+export const Links = ["About", "Skills", "Projects", "Contact"];
+
+export default function Footer() {
+  return (
+    <nav className="justify-self-end border-gray-200 bg-white bg-transparent px-2 py-2.5">
+      <div className="container mx-auto flex flex-col flex-wrap items-center justify-between gap-3 sm:flex-row">
+        <KasperBoxLogo className="w-40 fill-black dark:fill-white" />
+        <h3 className="text-slate-600 dark:text-slate-200">
+          © 2023 Kasper Luna. All rights reserved
+        </h3>
+        <div className="inline-flex items-center">
+          <Tooltip text="Check out my Github!">
+            <ButtonLink
+              icon={
+                <GithubIcon className="h-6 w-6 fill-black dark:fill-white" />
+              }
+              ariaLabel="Kasper's Github"
+              href={"https://github.com/KasperLuna"}
+            />
+          </Tooltip>
+          <Tooltip text="Connect with me on LinkedIn!">
+            <ButtonLink
+              icon={<LinkedInIcon />}
+              ariaLabel="Kasper's LinkedIn"
+              href={"https://www.linkedin.com/in/kasperluna/"}
+            />
+          </Tooltip>
+        </div>
+      </div>
+    </nav>
+  );
+}
