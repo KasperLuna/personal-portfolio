5f23db1b65976000feb7684a6ed736f632d5638f
Kasper Luna <dev@kasperluna.com>
2023-05-31 13:39:48 +0800

    add reduced motion styling

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -84,7 +84,7 @@ export default function Hero() {
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#About"
-                  className="animate-bounce rounded-3xl bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="animate-bounce motion-reduce:animate-none rounded-3xl bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Learn More
                 </a>
